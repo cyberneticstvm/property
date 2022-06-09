ae6faf983e0e17e773a8a91f380a9087a8281fef
CYBER NETICS <cybernetics.me@outlook.com>
2022-06-09 12:30:45 +0530

    initial commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,313 @@
+<!doctype html>
+<html lang="zxx">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/bootstrap.min.css' }}">
+		<!-- Owl Theme Default Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/owl.theme.default.min.css' }}">
+		<!-- Owl Carousel Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/owl.carousel.min.css' }}">
+		<!-- Boxicons Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/boxicons.min.css' }}"> 
+		<!-- Flaticon CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/flaticon.css' }}">
+		<!-- Meanmenu Min CSS -->
+		<link rel="stylesheet" href="{{ public_path().'/css/meanmenu.min.css' }}">
+		<!-- Animate Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/animate.min.css' }}">
+		<!-- Nice Select Min CSS -->
+		<link rel="stylesheet" href="{{ public_path().'/css/nice-select.min.css' }}">
+		<!-- Odometer Min CSS -->
+		<link rel="stylesheet" href="{{ public_path().'/css/odometer.min.css' }}">
+		<!-- Date Picker CSS-->
+		<link rel="stylesheet" href="{{ public_path().'/css/date-picker.min.css' }}">
+		<!-- Magnific Popup Min CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/magnific-popup.min.css' }}"> 
+		<!-- Beautiful Fonts CSS --> 
+		<link rel="stylesheet" href="{{ public_path().'/css/beautiful-fonts.css' }}">
+		<!-- Style CSS -->
+		<link rel="stylesheet" href="{{ public_path().'/css/style.css' }}">
+		<!-- Responsive CSS -->
+		<link rel="stylesheet" href="{{ public_path().'/css/responsive.css' }}">
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" href="{{ public_path().'/img/favicon.png' }}">
+		
+        <!-- TITLE -->
+        <title>Ecorik - Hotel & Resort HTML Template</title>
+    </head>
+
+    <body>
+		<!-- Start Preloader Area -->
+		<div class="preloader">
+			<div class="lds-ripple">
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+		<!-- End Preloader Area -->
+	
+		@yield("content")
+
+		<!-- Start Footer Menu Area -->
+		<div class="footer-menu-area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-4">
+						<div class="footer-logo">
+							<img src="{{ public_path().'/img/home-four/footer-logo.png' }}" alt="Image">
+						</div>
+					</div>
+					<div class="col-lg-8">
+						<ul>
+							<li>
+								<a href="about.html">
+									About Us
+								</a>
+							</li>
+							<li>
+								<a href="testimonial.html">
+									Testimonial
+								</a>
+							</li>
+							<li>
+								<a href="service-style-one.html">
+									Service
+								</a>
+							</li>
+							<li>
+								<a href="news-grid.html">
+									News Grid
+								</a>
+							</li>
+							<li>
+								<a href="news-details.html">
+									News Details
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Footer Menu Area -->
+
+		<!-- Start Footer Area -->
+		<footer class="footer-top-area footer-top-area-four">
+			<div class="container">
+				<div class="footer-middle-area pt-100 pb-70">
+					<div class="row">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Contact Info</h3>
+								<ul class="information">
+									<li class="address">
+										<i class="flaticon-maps-and-flags"></i>
+										<span>Address</span>
+										205 Fida Walinton, Tongo Street Front The USA
+									</li>
+									<li class="address">
+										<i class="flaticon-call"></i>
+										<span>Phone</span>
+										<a href="tel:+882-569-756">
+											+882-569-756
+										</a>
+									</li>
+									<li class="address">
+										<i class="flaticon-envelope"></i>
+										<span>Email</span>
+										<a href="mailto:hello@ecorik.com">
+											hello@ecorik.com
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Services</h3>
+								<ul>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Restaurant
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Swimming Pool
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Wellness & Spa
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Conference Room
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Events
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Adult Room
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Quick Links</h3>
+								<ul>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Big Data
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Wellness
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Spa Gallery
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Reservation
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											FAQ 
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Contact 
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Instagram</h3>
+								<ul class="instagram-wrap">
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/1.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/2.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/3.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/4.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/5.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/6.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/7.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/8.jpg' }}" alt="Image"></a>
+									</li>
+									<li>
+										<a href="#"><img src="{{ public_path().'/img/instagram/9.jpg' }}" alt="Image"></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bottom-area mt-0">
+					<div class="row align-items-center">
+						<div class="col-lg-6">
+							<div class="copy-right">
+								<p>Copyright <i class="bx bx-copyright"></i> 2020 <a href="index.html">Ecorik</a>. All Rights Reserved</p>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="designed">
+								<p>Designed By <i class='bx bx-heart'></i> <a href="https://envytheme.com/" target="_blank">EnvyTheme</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- End Footer Area -->
+
+		<!-- Start Go Top Area -->
+		<div class="go-top">
+			<i class='bx bx-chevrons-up bx-fade-up'></i>
+			<i class='bx bx-chevrons-up bx-fade-up'></i>
+		</div>
+		<!-- End Go Top Area --> 
+
+        
+        <!-- Jquery Min JS -->
+        <script src="{{ public_path().'/js/jquery.min.js' }}"></script> 
+        <!-- Bootstrap Bundle Min JS -->
+        <script src="{{ public_path().'/js/bootstrap.bundle.min.js' }}"></script>
+        <!-- Meanmenu Min JS -->
+		<script src="{{ public_path().'/js/meanmenu.min.js' }}"></script>
+		<!-- Owl Carousel Min JS -->
+		<script src="{{ public_path().'/js/owl.carousel.min.js' }}"></script>
+		<!-- Wow Min JS -->
+        <script src="{{ public_path().'/js/wow.min.js' }}"></script>
+        <!-- Nice Select Min JS -->
+		<script src="{{ public_path().'/js/nice-select.min.js' }}"></script>
+        <!-- Magnific Popup Min JS -->
+		<script src="{{ public_path().'/js/magnific-popup.min.js' }}"></script>
+		<!-- Mixitup JS --> 
+		<script src="{{ public_path().'/js/jquery.mixitup.min.js' }}"></script>
+		<!-- Appear Min JS --> 
+		<script src="{{ public_path().'/js/appear.min.js' }}"></script>
+		<!-- Odometer Min JS --> 
+		<script src="{{ public_path().'/js/odometer.min.js' }}"></script>
+		<!-- Datepicker Min JS --> 
+		<script src="{{ public_path().'/js/bootstrap-datepicker.min.js' }}"></script>
+		<!-- Ofi Min JS --> 
+		<script src="{{ public_path().'/js/ofi.min.js' }}"></script>
+		<!-- jarallax Min JS --> 
+		<script src="{{ public_path().'/js/jarallax.min.js' }}"></script>
+		<!-- Form Validator Min JS -->
+		<script src="{{ public_path().'/js/form-validator.min.js' }}"></script>
+		<!-- Contact JS -->
+		<script src="{{ public_path().'/js/contact-form-script.js' }}"></script>
+		<!-- Ajaxchimp Min JS -->
+		<script src="{{ public_path().'/js/ajaxchimp.min.js' }}"></script>
+        <!-- Custom JS -->
+		<script src="{{ public_path().'/js/custom.js' }}"></script>
+    </body>
+</html>
